5c5451d479136c30ab7e1f2a17661ab6a7e9bae7
Tanmai Raghava <tanmairaghav3836@gmail.com>
2025-06-06 19:16:56 +0530

    added nav component

---
+++ Hotspot/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import {link} from "react-router-dom";
+import "../css/Navbar.css";
+
+function NavBar() {
+    return <nav className='navbar'>
+        <div className='nav-brand'>
+            <link to="/">Hotspot</link>
+        </div>
+        <div className='nav-links'>
+            <link to="/" className="nav-link">Home</link>
+            <link to="/mapify" className="nav-link">Mapify</link>
+        </div>
+    </nav>
+}
+
+export default NavBar;
